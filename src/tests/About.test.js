96866f3c424fb5ca5bbf0d6093af36f9d46182d2
Amanda Portela <mandaptela@gmail.com>
2022-07-18 22:16:33 -0300

    Requisito 3 - Not Found page

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,23 +3,20 @@ import React from 'react';
 import About from '../pages/About';
 
 describe('Teste o componente About', () => {
-  render(<About />);
+  const { container } = render(<About />);
+  const paragrafos = container.querySelectorAll('p');
 
   test('Teste se a página contém as informações sobre a Pokédex;', () => {
-    const abouHeading = screen.getByRole('heading', {
+    const aboutHeading = screen.getByRole('heading', {
       name: /about pokédex/i,
       level: 2,
     });
-    expect(abouHeading).toBeInTheDocument();
+    expect(aboutHeading).toBeInTheDocument();
   });
 
   test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
     render(<About />);
-    const infoAbout = screen.getByText(/This application simulates a Pokédex/i);
-    expect(infoAbout).toBeInTheDocument();
-
-    const infoAbout2 = screen.getByText(/One can filter Pokémons by type/i);
-    expect(infoAbout2).toBeInTheDocument();
+    expect(paragrafos.length).toBe(2);
   });
 
   test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('Teste a página Not Found', () => {
+  const { container } = render(<NotFound />);
+  const image = container.querySelector('.not-found-image');
+
+  test('Verifica se página possui um H2 informando página não encontrada', () => {
+    const textHeading = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+    expect(textHeading).toBeInTheDocument();
+  });
+
+  test('Testa presença do Emoji', () => {
+    render(<NotFound />);
+    const emoji = screen.getByLabelText('Crying emoji');
+    expect(emoji).toBeInTheDocument();
+  });
+
+  test('Imagem Pikachu', () => {
+    render(<NotFound />);
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
