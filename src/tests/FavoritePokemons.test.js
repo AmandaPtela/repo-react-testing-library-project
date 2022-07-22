c6822be360fd9bc6c923607f7ac7b127a2da7fbc
Amanda Portela <mandaptela@gmail.com>
2022-07-21 23:38:58 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,23 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import React from 'react';
 import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../components/renderWithRouter';
+import App from '../App';
+import { readFavoritePokemonIds } from '../services/pokedexService';
 
 describe(' Testa página Favoritos', () => {
   beforeEach(() => {
-    render(<FavoritePokemons />);
+    renderWithRouter(<FavoritePokemons />);
   });
   test('Verifica se a função que pega os pokemons favoritos é chamada', async () => {
     const readFavoritePokemonIds = jest.fn();
     readFavoritePokemonIds();
     expect(readFavoritePokemonIds).toHaveBeenCalled();
-    // RETORNAR DAQUI
-    const { container } = render(<FavoritePokemons />);
-    const cardsPokemons = container.getElementsByClassName('.favorite-pokemon');
-    expect(cardsPokemons).toBeInTheDocument();
   });
-
-  test('Verifica se aparece mensagem sobre falta de favoritos', () => {
+  
+  test('Verifica se todos os cards são exibidos', () => {
+/*     if (readFavoritePokemonIds()) {
+      const alink = screen.getByText('More details');
+      const alinkAtt = alink.getAttribute('href').split('/');
+      const linkFav = alinkAtt[2] === 25;
+      expect(alink).toHaveAttribute('href', `/pokemons/${linkFav}`);
+    } */
     const textFavorites = screen.getByText(/No favorite pokemon found/i);
     expect(textFavorites).toBeInTheDocument();
-  });
+  })
 });
